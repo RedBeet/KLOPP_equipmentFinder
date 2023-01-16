84f7be9c3bb6903321806f8c75d7d22e5318f940
RedBeet <ohmingun9@gmail.com>
2023-01-16 19:34:13 +0900

    First commit

--- script.js
+++
@@ -1,9 +0,0 @@
-const floor = [null];
-for(let i = 1; i <= 5; i++){
- floor.push(document.querySelector(String(`#floor${i}`)));
- floor[i].addEventListener("click", () => { floorClick(i) })
-}
-
-function floorClick(n){
-    window.location.href=`floors/${n}F.html`;
-}
